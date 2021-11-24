7e7798f5b562e59e20f22764614b86d1dce22c47
mikechibuzor <iwuagwuchibuzor@gmail.com>
2021-11-24 16:04:36 +0100

    worked upto a point, worked on responsivenss

---
+++ javascript/script.js
@@ -0,0 +1,33 @@
+// code below handles the toggle effect of the active nav on the header section
+
+// selects all nav links and converts them to an array element right after
+const navLinks = [...document.querySelectorAll(".nav-link")];
+
+// attaches a click event listener to each nav link
+function navToggleHandler() {
+  navLinks.forEach((link) => {
+    link.addEventListener("click", navToggler);
+  });
+}
+
+function navToggler(event) {
+  // removes active class from previous link
+  navLinks.forEach((link) => {
+    if (link.classList.contains("active")) {
+      link.classList.remove("active");
+    }
+  });
+
+  // adds the active class to the clicked nav-link element
+  event.target.classList.add("active");
+}
+
+navToggleHandler();
+
+const hamburgerBtn = document.querySelector(".btnn");
+const navBar = document.querySelector("#nav-bar");
+
+hamburgerBtn.addEventListener("click", () => {
+  navBar.classList.toggle("move");
+  //   console.log(navLinks);
+});
